6c5eb75066c56ca199640eab4217130bcd01a18e
Serena Lertkantitham <lertkantitham@gmail.com>
2019-02-09 14:20:01 -0800

    updated dependencies

--- test/test-blogpost-integration.js
+++ test/test-blogpost-integration.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+
+const expect = chai.expect;
+
+const { BlogPost } = require('./models');
+const { app, runServer, closeServer } = require('./server');
+const { TEST_DATABASE_URL } = require('./config');
